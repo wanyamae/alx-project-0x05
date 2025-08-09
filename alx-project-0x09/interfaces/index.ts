200a764daa0f4da36a0268534fe11a7e9e8d028f
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-09 09:23:23 +0300

    Updated the codes

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,20 +1,20 @@
-import React from 'react';
+import { ReactNode } from "react";
 
 export interface ReactComponentProps {
-    children: React.ReactNode;
-    }
+  children: ReactNode
+}
 
 
 export interface GeneratedImageProps {
-    imageUrl: string;
-    prompt: string;
-    width?: string;
-    height?: string;
-    action: (imagePath: string) => void;
+  imageUrl: string
+  prompt: string
+  width?: string
+  height?: string
+  action: (imagePath: string) => void
 }
 
 export type RequestProps = {
-    prompt: string;
+  prompt: string;
 }
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
